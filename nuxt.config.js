cf0f7a1d6a3a4e74be0c8a306cd6cace610e7def
Richard Huf <richardhuf@gmhba.com.au>
2021-04-30 23:02:58 +1000

    Auto fix eslint errors

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,9 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    ['@nuxtjs/eslint-module', {
+      fix: true
+    }],
     '@nuxtjs/prismic'
   ],
 
