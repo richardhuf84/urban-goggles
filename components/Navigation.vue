cb657f013b10e92353fdf771d967b20931654093
Richard Huf <richardhuf@gmhba.com.au>
2021-04-30 23:03:23 +1000

    Configure navigation

---
+++ components/Navigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li><nuxt-link to="/">Home</nuxt-link></li>
+    <li><nuxt-link to="posts">Posts</nuxt-link></li>
+    <li>
+      Pages
+      <ul>
+        <li><nuxt-link to="/about">About</nuxt-link></li>
+        <li>
+          <nuxt-link to="/the-jamstack-is-lots-of-fun">JAMStack</nuxt-link>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  mounted() {
+    // console.log(this);
+  },
+};
+</script>
+
+<style scoped></style>
