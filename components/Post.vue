ac52b007a3150520bcb14cbf67718fceef5889e9
Richard Huf <>
2021-05-15 13:11:15 +1000

    feat: :fire: Improve posts

---
+++ components/Post.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="post">
+    <figure class="hero">
+      <prismic-image :field="post.data.post_hero" />
+      <figcaption class="hero__caption">
+        <prismic-rich-text :field="post.data.image_attribution" />
+      </figcaption>
+    </figure>
+
+    <div class="post__content">
+      <prismic-rich-text :field="post.data.post_title" />
+
+      <prismic-rich-text :field="post.data.post_body" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.hero {
+  margin: 0;
+  /* flex-basis: 33%; */
+  max-height: 14vw;
+  overflow: hidden;
+}
+
+.hero img {
+  max-width: 100%;
+  object-fit: cover;
+}
+
+.hero__caption {
+  padding: 0 20px;
+}
+
+.post {
+  display: flex;
+  box-shadow: 0 0 12px rgba(0, 0, 0, 0.4);
+  flex-direction: column;
+  margin: 20px;
+  padding-bottom: 1rem;
+}
+
+h1 {
+  font-size: 1.5rem;
+}
+
+.post__content {
+  flex-basis: 66%;
+  padding: 20px;
+  max-height: 30vw;
+  overflow: hidden;
+  position: relative;
+}
+
+.post__content::after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 3rem;
+  background: linear-gradient(
+    to bottom,
+    rgba(255, 255, 255, 0),
+    rgba(255, 255, 255, 1)
+  );
+}
+</style>
--- components/Posts.vue
+++ components/Posts.vue
@@ -1,19 +1,8 @@
 <template>
   <div class="posts">
     <h1>Posts</h1>
-    <div v-for="(post, index) in posts" :key="index" class="post">
-      <figure class="hero">
-        <prismic-image :field="post.data.post_hero" />
-        <figcaption class="hero__caption">
-          <prismic-rich-text :field="post.data.image_attribution" />
-        </figcaption>
-      </figure>
-
-      <div class="post__content">
-        <prismic-rich-text :field="post.data.post_title" />
-
-        <prismic-rich-text :field="post.data.post_body" />
-      </div>
+    <div class="grid-layout">
+      <Post v-for="(post, index) in posts" :key="index" :post="post" />
     </div>
   </div>
 </template>
@@ -36,29 +25,9 @@ export default {
 </script>
 
 <style scoped>
-.posts {
-  max-width: 65vw;
-  text-align: left;
-}
-
-.hero {
-  margin: 0;
-  flex-basis: 33%;
-}
-
-.hero__caption {
-  padding: 0 20px;
-}
-
-.post {
-  display: flex;
-  box-shadow: 0 0 12px rgba(0, 0, 0, 0.4);
-  flex-direction: column;
-  margin: 20px;
-}
-
-.post__content {
-  flex-basis: 66%;
-  padding: 20px;
+.grid-layout {
+  display: grid;
+  grid-template-rows: auto;
+  grid-template-columns: 1fr 1fr 1fr;
 }
 </style>
